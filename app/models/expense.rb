9b821d9a626987f157e633da892b60a366d6dbcf
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-13 19:48:43 +0000

    Validate expense attributes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,8 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_expenses, dependent: :destroy
   has_many :groups, through: :group_expenses
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :amount, presence: true, numericality: true
+  validates :author, presence: true
 end

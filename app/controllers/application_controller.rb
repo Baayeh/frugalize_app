dafd9a3c0b93287ee4cdd9bea7895a5b8f13901c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-14 06:20:33 +0000

    Fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update , keys: [:name, :email, :password, :current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
   end
 end

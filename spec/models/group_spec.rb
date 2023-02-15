ab84434a0f309f0bfbcca72ba296cd02bd4346a5
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-15 13:00:51 +0000

    Unit test for group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { create(:user) }
+  let(:group) { create(:group, author:) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:author) }
+    it { is_expected.to have_many(:group_expenses) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(5) }
+    it { is_expected.to validate_presence_of(:icon) }
+    it { is_expected.to validate_presence_of(:author) }
+  end
 end

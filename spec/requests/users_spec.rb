9395b60fb623f01fe1b75eee668e466a8425aca2
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-15 13:12:09 +0000

    Tests for Users Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe UsersController, type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
+      get users_path
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns a success response' do
+      get new_user_registration_path
+      expect(response).to be_successful
     end
   end
 end
